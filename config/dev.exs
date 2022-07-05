3a49748679c482baa71f020be728050259d3f0d6
Nelson Estevão <nelsonmestevao@proton.me>
2022-07-05 10:34:15 +0100

    Change databases localization

--- config/dev.exs
+++ config/dev.exs
@@ -2,7 +2,7 @@ import Config
 
 # Configure your database
 config :quizium, Quizium.Repo,
-  database: Path.expand("../quizium_dev.db", Path.dirname(__ENV__.file)),
+  database: Path.expand("../priv/repo/databases/quizium_dev.db", Path.dirname(__ENV__.file)),
   pool_size: 5,
   stacktrace: true,
   show_sensitive_data_on_connection_error: true
--- config/test.exs
+++ config/test.exs
@@ -6,7 +6,7 @@ import Config
 # to provide built-in test partitioning in CI environment.
 # Run `mix help test` for more information.
 config :quizium, Quizium.Repo,
-  database: Path.expand("../quizium_test.db", Path.dirname(__ENV__.file)),
+  database: Path.expand("../priv/repo/databases/quizium_test.db", Path.dirname(__ENV__.file)),
   pool_size: 5,
   pool: Ecto.Adapters.SQL.Sandbox
 
